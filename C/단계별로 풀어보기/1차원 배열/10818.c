a32353bf38e801818d30c80085ed2e519883bd20
yoosh199 <64148999+yoosh199@users.noreply.github.com>
2021-04-19 00:08:44 +0900

    solve 10818

---
+++ C/단계별로 풀어보기/1차원 배열/10818.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n, a, max, min;
+    max = -1000001;
+    min = 1000001;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a);
+        if (a > max)
+        {
+            max = a;
+        }
+        if (a < min)
+        {
+            min = a;
+        }
+    }
+
+    printf("%d %d", min, max);
+    return 0;
+}
