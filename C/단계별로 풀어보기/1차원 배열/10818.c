cca6fa687e82191916ea30633b4698837cd81816
yoosh199 <64148999+yoosh199@users.noreply.github.com>
2021-04-19 18:42:04 +0900

    Update 10818.c

--- C/단계별로 풀어보기/1차원 배열/10818.c
+++ C/단계별로 풀어보기/1차원 배열/10818.c
@@ -3,24 +3,44 @@
 
 int main()
 {
-    int n, a, max, min;
+    int n, max, min;
     max = -1000001;
     min = 1000001;
+
     scanf("%d", &n);
+    int arr[n];
+    for (int j = 0; j < n; j++)
+    {
+        scanf("%d", &arr[j]);
+    }
 
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", &a);
-        if (a > max)
+        if (arr[i] > max)
         {
-            max = a;
+            max = arr[i];
         }
-        if (a < min)
+        if (arr[i] < min)
         {
-            min = a;
+            min = arr[i];
         }
     }
 
     printf("%d %d", min, max);
     return 0;
 }
+
+// for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &a);
+//         if (a > max)
+//         {
+//             max = a;
+//         }
+//         if (a < min)
+//         {
+//             min = a;
+//         }
+//     }
+
+//     printf("%d %d", min, max);
