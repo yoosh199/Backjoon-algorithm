005e4e86364e3c152ad9d054f34a19405e8f1f62
Si-Hyeon Yoo <64148999+yoosh199@users.noreply.github.com>
2022-02-19 16:29:50 +0900

    Create j_11654.java

---
+++ C/단계별로 풀어보기/문자열/j_11654.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class j_11654 {
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+
+        String a =in.nextLine();
+    
+        
+        System.out.printf("%d", (int)a.charAt(0));
+
+        in.close();
+    }
+}
