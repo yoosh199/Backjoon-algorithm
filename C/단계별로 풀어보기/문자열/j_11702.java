498d86c70878a11018651ca69bd5f1a99da68c16
Si-Hyeon Yoo <64148999+yoosh199@users.noreply.github.com>
2022-04-01 00:03:04 +0900

    Update j_11702.java

--- C/단계별로 풀어보기/문자열/j_11702.java
+++ C/단계별로 풀어보기/문자열/j_11702.java
@@ -5,7 +5,7 @@ public class j_11702 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        in.nextLine();
+        in.nextLine();//여기서 int 바로 받고 nextline 안됨
         String nn = in.nextLine();
         int sum=0;
         for(int i=0;i<n;i++){
