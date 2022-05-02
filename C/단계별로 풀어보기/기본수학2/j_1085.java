b362d7d01860c0213e78c7f0a59a975f099628ea
Si-Hyeon Yoo <64148999+yoosh199@users.noreply.github.com>
2022-05-02 22:11:48 +0900

    Create j_1085.java

---
+++ C/단계별로 풀어보기/기본수학2/j_1085.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+import javax.xml.crypto.dsig.keyinfo.X509Data;
+
+public class j_1085 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+      
+        int x = in.nextInt();
+        int y = in.nextInt();
+        int w = in.nextInt();
+        int h = in.nextInt();
+        int min = 1000;
+        //상하좌우 다 계산하기
+        //up
+        if(h-y<min) min= h-y;
+
+        //down
+        if(y<min) min=y;
+
+        //left
+        if(x<min) min=x;
+
+        //right
+        if(w-x<min) min=w-x;
+
+        System.out.println(min);
+
+        in.close();
+    }
+}
