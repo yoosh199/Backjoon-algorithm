4cb4dcdb586b3db26483105913ddfe0b5adba982
Si-Hyeon Yoo <64148999+yoosh199@users.noreply.github.com>
2022-05-06 14:21:16 +0900

    Update j_1085.java

--- C/단계별로 풀어보기/기본수학2/j_1085.java
+++ C/단계별로 풀어보기/기본수학2/j_1085.java
@@ -1,7 +1,4 @@
 import java.util.Scanner;
-
-import javax.xml.crypto.dsig.keyinfo.X509Data;
-
 public class j_1085 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
