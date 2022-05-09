2c0447ee90c0544cec9d09129116df206eec8443
Si-Hyeon Yoo <64148999+yoosh199@users.noreply.github.com>
2022-05-09 22:35:26 +0900

    Create j_11729.java

---
+++ C/단계별로 풀어보기/재귀/j_11729.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+
+public class j_11729 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        
+        in.close();
+    }
+}
