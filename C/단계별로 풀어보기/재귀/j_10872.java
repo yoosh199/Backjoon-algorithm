3cd63c1edbcc8cc8d0c8c4458ad69e1c35b3b34c
Si-Hyeon Yoo <64148999+yoosh199@users.noreply.github.com>
2022-05-09 22:35:22 +0900

    Update j_10872.java

--- C/단계별로 풀어보기/재귀/j_10872.java
+++ C/단계별로 풀어보기/재귀/j_10872.java
@@ -2,18 +2,23 @@ import java.util.Scanner;
 
 public class j_10872 {
 
+    public static int pactolia(int n){
+
+        if(n==1||n==0){
+            return 1;
+        }
+
+        return n*pactolia(n-1);
+    }
+
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int pacto=1;
 
         
-        while(n>0){
-                pacto = n *pacto;
-                n--;
-        }
         
-        System.out.println(pacto);
+        System.out.println(pactolia(n));
         
        
         in.close();
