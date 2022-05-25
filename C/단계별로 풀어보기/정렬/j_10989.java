8a0687e084185d0274c596841a930ea8db83ed09
Si-Hyeon Yoo <64148999+yoosh199@users.noreply.github.com>
2022-05-25 22:13:30 +0900

    Update j_10989.java

--- C/단계별로 풀어보기/정렬/j_10989.java
+++ C/단계별로 풀어보기/정렬/j_10989.java
@@ -24,14 +24,16 @@ public class j_10989 {
             counting[i+1] += counting[i];
         }
 
-
+        //알고리즘 아이디어는 누적합을 계산해서 들어가 위치에 집어넣는것
+        //ex 5 2 3 1 4 2 3 5 1 7 이면
+        //   2 4 6 7 9 9 10 10 10 10 인데 7의 경우 10 즉 맨 뒤에 올수 밖에 없다 뒤에 8 9 10 인 10 10 10은 안쓰임
         for(int i=n-1;i>=0;i--){
-          
+
             counting[arr[i]-1] -= 1;
             answer[counting[arr[i]-1]]= arr[i];
         }
         
-
+        
         for(int i=0;i<n;i++){
           sb.append(answer[i]).append("\n");
         }
